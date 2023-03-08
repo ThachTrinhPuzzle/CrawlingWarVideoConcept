0f075b0097442fa30b520402ef042e3d5c3154d1
ThachTrinhPuzzle <106643654+ThachTrinhPuzzle@users.noreply.github.com>
2023-03-08 10:13:25 +0700

    skybox

---
+++ Assets/Scripts/VibrationTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class VibrationTest : MonoBehaviour
+{
+    public Transform castle;
+    public float duration;
+    public float strength;
+    public int vibrato;
+    public int id;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown((KeyCode.Alpha1 + id)))
+        {
+            if (id == 0) castle.DOShakePosition(duration, strength, vibrato);
+            else if (id == 1) castle.DOShakeRotation(duration, strength, vibrato);
+            else castle.DOShakeScale(duration, strength, vibrato);
+        }
+    }
+}
